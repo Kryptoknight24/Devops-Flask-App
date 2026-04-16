a70f52f0ab0fb72199772f4f0dc74fbc7c7164ad
Divyanshu Mandal <divyanshumanmandal@gmailcom>
2026-04-16 21:30:21 +0530

    Added Flask app, Dockerfile, and Docker Compose

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+import pymysql
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    try:
+        # We try to connect to the database
+        connection = pymysql.connect(
+            host='mysql-db', # This name matches the Docker Compose file!
+            user='root',
+            password='rootpassword',
+            database='myappdb'
+        )
+        return "Hello! The Flask App is running AND successfully connected to the MySQL Database!"
+    except Exception as e:
+        return f"Hello! The web app is running, but we are still waiting on the database: {e}"
+    finally:
+        if connection:
+            try:
+                connection.close()
+            except Exception:
+                # ignore errors when closing the connection
+                pass
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
